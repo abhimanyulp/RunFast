5647556a47fa531cfe82aa97b20bfeb3e0a5f501
Abhimanyu <lp.abhimanyu@gmail.com>
2023-08-19 00:07:12 +0530

    Product page done!

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,7 +1,5 @@
 const express = require("express")
 require("dotenv").config()
-// const bcrypt = require("bcrypt")
-// const jwt = require("jsonwebtoken")
 
 const productRouter = express.Router()
 const { productModel } = require("../models/product.model")
@@ -9,10 +7,10 @@ const { productModel } = require("../models/product.model")
 
 productRouter.post("/add", async (req, res) => {
 
-    const {  } = req.body
+    const { } = req.body
 
     try {
-        res.status(200).send({ "msg": "",})
+        res.status(200).send({ "msg": "", })
 
     } catch (error) {
         res.status(400).send({ "msg": error.message })
@@ -20,14 +18,68 @@ productRouter.post("/add", async (req, res) => {
 
 })
 
-productRouter.get("/get", async (req, res) => {
+productRouter.get("/data", async (req, res) => {
+
+    const page = parseInt(req.query._page) || 1;
+    const limit = parseInt(req.query._limit) || 10;
+    const sortField = req.query._sort || '_id';
+    const sortOrder = req.query._order === 'desc' ? -1 : 1;
+    const maxPrice = parseFloat(req.query.price);
 
     try {
-        const data = await productModel.find()
-        res.status(200).send({ "msg": "Data retrieved!", data })
 
+        const filters = {};
+
+        // Add filters based on query parameters
+        if (req.query.price) {
+            filters.price = { $lte: parseFloat(req.query.price) };
+        }
+        if (req.query.brand) {
+            filters.brand = {
+                $regex: req.query.brand,      // Regular expression pattern
+                $options: 'i'                 // Case-insensitive option
+            };
+        }
+        if (req.query.rating) {
+            filters.rating = parseFloat(req.query.rating);
+        }
+        if (req.query.color) {
+            filters.color = {
+                $regex: req.query.color,      // Regular expression pattern
+                $options: 'i'                 // Case-insensitive option
+            };
+        }
+
+        const pipeline = [
+            {
+                $match: filters,
+            },
+            {
+                $sort: { [sortField]: sortOrder },
+            },
+            {
+                $skip: (page - 1) * limit,
+            },
+            {
+                $limit: limit,
+            },
+        ];
+
+        const data = await productModel.aggregate(pipeline);
+
+        const totalItems = await productModel.countDocuments(filters);
+        const totalPages = Math.ceil(totalItems / limit);
+
+        console.log("someone was there!", "page:", page, "limit:", limit, "sortField:", sortField, "sortOrder:", sortOrder, "maxPrice:", maxPrice, "totalItems:", totalItems)
+
+        res.json({
+            data,
+            totalPages,
+            totalItems,
+            currentPage: page,
+        });
     } catch (error) {
-        res.status(400).send({ "msg": error.message })
+        res.status(500).json({ error: 'An error occurred' });
     }
 
 })
