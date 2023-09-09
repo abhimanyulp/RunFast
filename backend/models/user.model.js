363605252c0356f117e7123078f2d19406cea69d
Pranay <pmcanvas4501@gmail.com>
2023-09-09 15:09:13 +0530

    cart routes and mongoose  populate added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose")
 const userSchema = mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    mycart: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'product'
+    }]
 }, {
     versionKey: false
 })
---
+++ backend/routes/cart.routes.js
@@ -0,0 +1,40 @@
+const { userModel } = require("../models/user.model");
+const { CartModel } = require("../models/cart.model");
+const express = require('express');
+const cartRouter = express.Router();
+
+cartRouter.post('/', async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        const productId = req.body.productId;
+
+        const user = await userModel.findByIdAndUpdate(
+            userId,
+            { $push: { mycart: productId } },
+            { new: true }
+        ).exec();
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+cartRouter.get("/", async (req, res) => {
+    try {
+        const userId = req.body.userId;
+
+        const user = await userModel.findById(userId).populate('mycart').exec();
+        res.status(200).json(user.mycart);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+module.exports = { cartRouter };
+
+
+
