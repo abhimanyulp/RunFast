c4d73aa2e8e7e295632dc06f72fbede552d382b0
Pranay <pmcanvas4501@gmail.com>
2023-09-10 17:05:47 +0530

    logout route with blacklisting is added

---
+++ backend/models/blacklist.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const blacklistSchema = mongoose.Schema({
+    token: { type: String, required: true }
+}, {
+    versionKey: false
+})
+
+const BlacklistModel = mongoose.model("blacklist", blacklistSchema)
+
+module.exports = { BlacklistModel }
